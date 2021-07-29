f2720fa51a14af82ed536b8a8934063f5950198a
Joe Dodgson <j.dodgson@live.co.uk>
2021-07-29 22:29:24 +0100

    Move file creation into questions.js

--- controller.js
+++ controller.js
@@ -1,14 +1,10 @@
 // Require in node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
 
 // Require in local files
 const { InputQuestion, ListQuestion } = require("./helper");
 const { questionData } = require("./questions");
-
-// Promisify the writeFile function
-const writeFileAsync = util.promisify(fs.writeFile);
 
 const createReadme = async () => {
     try {
@@ -31,7 +27,7 @@ const createReadme = async () => {
                     question = new ListQuestion(text, questionKey, options);
                     break;
                 case 'Action':
-                    questionData[questionKey].action();
+                    questionData[questionKey].action(responseObj);
                     break;
                 default:
                     throw `Question type ${type} not handled`;
@@ -53,18 +49,6 @@ const createReadme = async () => {
             // Feed the response into the nextQuestion method
             nextQuestionKey = await questionData[questionKey].nextQuestion(responseValue);
         }
-
-        // Use the Github username and repo name to form a license shield URL
-        const licenseShieldMD = `![GitHub](https://img.shields.io/github/license/${answers.username}/${answers.repo}?logoColor=%23ff0000)`
-
-        // Create a readme file and write the content based on user input
-        const readmeContent = 
-        `# ${answers.title}\n\n## Table of contents:\n1. Description\n2. Installation\n3. Usage\n4. Author\n5. Contributing\n6. License\n7. Tests\n8. Contact\n\n## 1. Description:\n${answers.description}\n\n## 2. Installation:\n${answers.installation}\n\n## 3. Usage:\n${answers.usage}\n\n## 4. Author:\n${answers.name}\nGithub username: ${answers.username}\n<img src="${data.avatar_url}">\n\n## 5. Contributing:\n${answers.contributing}\n\n## 6. License:\n\n${licenseShieldMD}\n\n## 7. Tests:\nThe project passed the following tests:\n${answers.tests}\n\n## 8. Contact:\nFor any questions about this project, please contact ${answers.name} at the following email address:\n${email.address}`;
-        
-        const file = await writeFileAsync(readmeFilePath, readmeContent);
-
-        // Give a message to tell the user the file has been created.
-        console.log("Your README file was created successfully!");
     }
     catch (error) {
         console.log(`Error - controller.js - createReadme() - ${error}`);
--- questions.js
+++ questions.js
@@ -2,6 +2,10 @@
 const axios = require("axios");
 const validator = require("validator");
 const fs = require("fs");
+const util = require("util");
+
+// Promisify the writeFile function
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Questions data
 let questionData = {
@@ -30,7 +34,7 @@ let questionData = {
     'checkFileExists': {
         'text': null,
         'type': 'Action',
-        'action': () => {},
+        'action': responseObj => {},
         'nextQuestion': response => {
             // TODO - properly format the file path
             const readmeFilePath = `./Generated_README/${response['fileName']}`;
@@ -108,7 +112,7 @@ let questionData = {
         'type': 'Input',
         'nextQuestion': response => {
             // Check if the user has entered a valid email address. 
-            let validEmail = validator.isEmail(alternativeEmail.address);
+            let validEmail = validator.isEmail(response);
 
             // If user has not entered a valid email address, ask them to enter a different one
             if (validEmail) {
@@ -123,14 +127,14 @@ let questionData = {
         'text': 'Enter the name of the project repository as it appears on Github:',
         'type': 'Input',
         'nextQuestion': response => {
-            return 'description';
+            return 'projectTitle';
         }
     },
     'projectTitle': {
         'text': 'Enter a project title:',
         'type': 'Input',
         'nextQuestion': response => {
-            return 'githubRepo';
+            return 'description';
         }
     },
     'description': {
@@ -171,9 +175,44 @@ let questionData = {
     'createFile': {
         'text': null,
         'type': 'Action',
-        'action': () => {},
-        'nextQuestion': response => {
-        }
+        'action': async responseObj => {
+            try {
+                // Define filepath using filename
+                const readmeFilePath = `./Generated_README/${responseObj.filename}`;
+
+                // Use the Github username and repo name to form a license shield URL
+                const licenseShieldMD = `![GitHub](https://img.shields.io/github/license/${responseObj.githubUser}/${responseObj.githubRepo}?logoColor=%23ff0000)`
+                
+                // Create a readme file and write the content based on user input
+                const title = `# ${responseObj.projectTitle}\n\n`;
+                const toc = `## Table of contents:\n1. Description\n2. Installation\n3. Usage\n4. Author\n5. Contributing\n6. License\n7. Tests\n8. Contact\n\n`;
+                const description = `## 1. Description:\n${responseObj.description}\n\n`;
+                const installation = `## 2. Installation:\n${responseObj.installation}\n\n`;
+                const usage = `## 3. Usage:\n${responseObj.usage}\n\n`;
+                const avatar_url = `https://github.com/${responseObj.githubUser}.png`
+                const author = `## 4. Author:\n${responseObj.username}\n\nGithub username: ${responseObj.githubUser}\n\n<img src="${avatar_url}">\n\n`;
+                const contributing = `## 5. Contributing:\n${responseObj.contributing}\n\n`;
+                const license = `## 6. License:\n${licenseShieldMD}\n\n`;
+                const testing = `## 7. Testing:\n${responseObj.testing}\n\n`;
+                let contact = `## 8. Contact:\nFor any questions about this project, please contact ${responseObj.username} at the following email address:\n`;
+                if (responseObj.githubEmail === 'Yes') {
+                    contact += responseObj.githubEmail;
+                } 
+                else {
+                    contact += responseObj.contactEmail;
+                }
+                const readmeContent = title + toc + description + installation + usage + author + contributing + license + testing + contact;
+                
+                const file = await writeFileAsync(readmeFilePath, readmeContent);
+
+                // Give a message to tell the user the file has been created.
+                console.log("Your README file was created successfully!");
+            }
+            catch (error) {
+                console.log(`Error - questions.js - questionData.createFile.action() - ${error}`);
+            }
+        },
+        'nextQuestion': response => {}
     },
 }
 
